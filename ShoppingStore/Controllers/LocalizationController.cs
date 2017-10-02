bc4d81452dfe6c8b2178a33133fb2caf440b39d4
MartinWang <martinwang7963@gmail.com>
2017-10-02 16:00:25 +0800

    Add Delete  and Add Function to localization

--- ShoppingStore/Controllers/LocalizationController.cs
+++ ShoppingStore/Controllers/LocalizationController.cs
@@ -7,6 +7,7 @@ using AspNetCore.JsonLocalization;
 using Newtonsoft.Json;
 using ShoppingStore.Models.LocalizedViewModels;
 using PaginationTagHelper.Extensions;
+using System.Text.RegularExpressions;
 
 namespace ShoppingStore.Controllers
 {
@@ -21,7 +22,8 @@ namespace ShoppingStore.Controllers
         public IActionResult Index(LocalizedViewModel model)
         {
 
-            var jsonLocalizedPaging = jsonLocalization.ToPageList(model.Page, model.ItemPerPage);
+            var jsonLocalizedPaging = 
+                jsonLocalization.ToPageList(model.Page, model.ItemPerPage);
 
             var jsonData = new LocalizedViewModel
             {
@@ -34,15 +36,98 @@ namespace ShoppingStore.Controllers
         }
 
         [HttpPost]
+        [Route("api/addNewKeyName")]
+        public IActionResult AddNewKeyName(string pk, string culture, string value)
+        {
+            if (String.IsNullOrWhiteSpace(pk))
+            {
+                return BadRequest("KeyName can't be empty");
+            }
+
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            if (!regexItem.IsMatch(pk))
+            {
+                return BadRequest("Only words and numbers");
+            }
+
+            if (String.IsNullOrWhiteSpace(culture) || String.IsNullOrWhiteSpace(value))
+            {
+                var onlyKeyName = new JsonLocalizationFormat
+                {
+                    Key=pk
+                };
+                jsonLocalization.Add(onlyKeyName);
+
+                var onlyKeyName_output = JsonConvert.SerializeObject(jsonLocalization, Formatting.Indented);
+                System.IO.File.WriteAllText(@"Resources/localization.json", onlyKeyName_output);
+
+                return Ok();
+            }
+
+            var newData = new JsonLocalizationFormat
+            {
+                Key = pk,
+                Value = new Dictionary<string, string>
+                {
+                    [culture] = value
+                }
+            };
+
+            jsonLocalization.Add(newData);
+
+            var output = JsonConvert.SerializeObject(jsonLocalization, Formatting.Indented);
+            System.IO.File.WriteAllText(@"Resources/localization.json", output);
+
+            return Ok();
+        }
+
+        [HttpPost]
         [Route("api/editKeyName")]
         public IActionResult EditKeyName(string pk, string value)
         {
-            if (value == null)
+            if (String.IsNullOrWhiteSpace(value))
             {
-                return BadRequest();
+                return BadRequest("Can't be empty or white space");
             }
+
+            if (String.IsNullOrWhiteSpace(pk))
+            {
+                return NotFound(pk + " Not Found");
+            }
+
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            if (!regexItem.IsMatch(pk))
+            {
+                return BadRequest("Only words and number");
+            }
+
             jsonLocalization.Where(data => data.Key == pk)
                 .ToList().ForEach(d => d.Key = value);
+
+            var output = JsonConvert.SerializeObject(jsonLocalization, Formatting.Indented);
+            System.IO.File.WriteAllText(@"Resources/localization.json", output);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("api/deleteKeyName")]
+        public IActionResult DeleteKeyName(string pk)
+        {
+            if (String.IsNullOrWhiteSpace(pk))
+            {
+                return NotFound();
+            }
+
+            var keyName = jsonLocalization.FirstOrDefault(p => p.Key == pk);
+            if (keyName == null)
+            {
+                return NotFound(pk + " not found");
+            }
+
+            jsonLocalization.Remove(keyName);
 
             var output = JsonConvert.SerializeObject(jsonLocalization, Formatting.Indented);
             System.IO.File.WriteAllText(@"Resources/localization.json", output);
@@ -54,12 +139,20 @@ namespace ShoppingStore.Controllers
         [Route("api/addNewCulture")]
         public IActionResult AddNewCulture(string pk, string culture, string value)
         {
-            if (culture == null)
+            if (String.IsNullOrWhiteSpace(culture))
             {
-                return BadRequest("Culture value can't be empty");
+                return BadRequest("Culture value can't be empty or white space");
+            }
+
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+
+            if (!regexItem.IsMatch(culture))
+            {
+                return BadRequest("Only letters and numbers");
             }
 
             var jsonData = jsonLocalization.Where(data => data.Key == pk).ToList();
+
             foreach (var d in jsonData)
             {
                 if (d.Value.Keys.Any(k => k.Equals(culture, StringComparison.OrdinalIgnoreCase)))
@@ -79,7 +172,7 @@ namespace ShoppingStore.Controllers
         [Route("api/deleteCulture")]
         public IActionResult DeleteCulture(string culture, string value)
         {
-            if (culture == null)
+            if (String.IsNullOrWhiteSpace(culture))
             {
                 return BadRequest("Culture doesn't exist");
             }
@@ -89,7 +182,6 @@ namespace ShoppingStore.Controllers
             {
                 data.Value.Remove(culture);
             }
-
 
             var output = JsonConvert.SerializeObject(jsonLocalization, Formatting.Indented);
             System.IO.File.WriteAllText(@"Resources/localization.json", output);
@@ -101,6 +193,10 @@ namespace ShoppingStore.Controllers
         [Route("api/editValue")]
         public IActionResult EditLocalizedValue(string pk, string name, string value)
         {
+            if (String.IsNullOrWhiteSpace(pk))
+            {
+                return NotFound();
+            }
 
             jsonLocalization.Where(data => data.Key == pk)
               .ToList().ForEach(d => d.Value[name] = value);
